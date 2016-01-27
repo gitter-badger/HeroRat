896c5138f979a354d1c39fcda88d0a0ec4c780ea
Joseph Laycock <joseph.laycock94@gmail.com>
2016-01-28 00:16:12 +1100

    Added database creation script

--- HeroRat_Server/src/net/herorat/features/dos/DosInterface.java
+++
@@ -1,7 +0,0 @@
-package net.herorat.features.dos;
-
-public interface DosInterface
-{
-	public void start();
-	public void stop();
-}
--- HeroRat_Server/src/org/jnativehook/mouse/NativeMouseInputListener.java
+++
@@ -1,7 +0,0 @@
-package org.jnativehook.mouse;
-
-import org.jnativehook.mouse.NativeMouseListener;
-import org.jnativehook.mouse.NativeMouseMotionListener;
-
-public interface NativeMouseInputListener extends NativeMouseListener, NativeMouseMotionListener {
-}
